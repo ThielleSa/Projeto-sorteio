d0a77269e7028d456cbd62ed3073cd9ee2d4acb2
ThielleSa <145806511+ThielleSa@users.noreply.github.com>
2024-12-03 13:01:15 -0300

    Update login.blade.php

--- Sorteador/resources/views/login.blade.php
+++ Sorteador/resources/views/login.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
     <title>Login - Sorteador</title>
     <!-- Link do Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-
 
     <!-- Estilos personalizados (se necessário) -->
     <style>
@@ -35,29 +34,42 @@
     </style>
 </head>
 <body>
+
 <div class="login-container">
     <!-- Logo acima do formulário -->
     <div class="text-center mb-4">
         <img src="images/logo.png" alt="Logo" class="img-fluid" style="max-width: 150px;">
         <h3 style="color: #3854b2;"><i>Sorteador</i></h3>
     </div>
-    <form>
+
+    <form method="post" action="{{ route('logar') }}">
+        @csrf
+
+        <!-- Campo de username -->
         <div class="mb-3">
             <label for="username" class="form-label">Nome de usuário</label>
-            <input type="text" class="form-control" id="username" placeholder="Digite seu nome de usuário" required>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Digite seu nome de usuário" required>
         </div>
+
+        <!-- Campo de password -->
         <div class="mb-3">
             <label for="password" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="password" placeholder="Digite sua senha" required>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Digite sua senha" required>
         </div>
-        <div class="d-flex justify-content-between">
-            <div>
-                <input type="checkbox" id="rememberMe"> <label for="rememberMe">Lembrar de mim</label>
+
+        <!-- Exibindo erro de login, caso exista -->
+        @if ($errors->has('login_error'))
+            <div class="alert alert-danger">
+                {{ $errors->first('login_error') }}
             </div>
+        @endif
+
+        <div class="d-flex justify-content-center">
             <div>
                 <a href="#">Esqueceu a senha?</a>
             </div>
         </div>
+
         <button type="submit" class="btn btn-primary w-100 mt-3">Entrar</button>
     </form>
 </div>
