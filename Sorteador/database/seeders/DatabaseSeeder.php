9669f310d136407903f88dcf9b1c9beb0f12f029
ThielleSa <145806511+ThielleSa@users.noreply.github.com>
2024-12-03 13:02:04 -0300

    Update DatabaseSeeder.php

--- Sorteador/database/seeders/DatabaseSeeder.php
+++ Sorteador/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(AdminUserSeeder::class);  // Adicionando o seeder do usuário admin
     }
 }
